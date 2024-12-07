d5ee03d51f2e87bcad60609a870554ace3710438
museonethree <2571947640@qq.com>
2024-12-07 17:59:56 +0800

    ui脚本测试

--- Metaboxingt3/Assets/C#/UIManager.cs
+++ Metaboxingt3/Assets/C#/UIManager.cs
@@ -10,11 +10,25 @@ public class UIManager : Singleton<UIManager>
         Debug.Log("UIManager已实例化");
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<GameObject> uiObjects = new List<GameObject>();
+    public int uiLenth ;    // 存放所有 UI 元素的列表
+
+    // 显示指定 UI
+    public void ShowUI(int uiNumber)
     {
-        
+        if(uiNumber<=uiLenth)
+            uiObjects[uiNumber].SetActive(true);
+
     }
+
+    // 隐藏指定 UI
+    public void HideUI(int uiNumber)
+    {
+        if (uiNumber <= uiLenth)
+            uiObjects[uiNumber].SetActive(false);
+
+    }
+
 
 
 
